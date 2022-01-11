4e74e31e90e6102a9415f3bbcea88d21622c922d
Jacques Fourie <jakfourie1@gmail.com>
2022-01-11 11:10:11 +0200

    Add files via upload

---
+++ SingleStakeCompound.js
@@ -0,0 +1,96 @@
+require('dotenv').config()
+const Web3 = require('web3')
+const web3 = new Web3('https://polygon-mainnet.infura.io/v3/7a2d659fe6604d0cb3e02cd329626792')
+var Tx = require("ethereumjs-tx").Transaction
+var Common = require('ethereumjs-common').default;
+
+const commonMatic = Common.forCustomChain(
+  'mainnet',
+  {
+    name: 'matic-mainnet',
+    networkId: 137,
+    chainId: 137,
+    url: 'https://rpc-mainnet.maticvigil.com/'
+  },
+  'petersburg'
+)
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//Setup Ends
+//Functions Start
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+async function Harvest(account, privateKey, contractAddress, contractABI, poolID){
+
+  const contract = new web3.eth.Contract(contractABI, contractAddress)
+  web3.eth.getTransactionCount(account, (err, txCount) => {
+
+    const txObject = {
+      nonce:    web3.utils.toHex(txCount),
+      gasLimit: web3.utils.toHex(800000), // Raise the gas limit to a much higher amount
+      gasPrice: web3.utils.toHex(web3.utils.toWei('10', 'gwei')),
+      to: contractAddress,
+      data: contract.methods.deposit(poolID, 0).encodeABI()
+    }
+
+    const tx = new Tx(txObject, {common : commonMatic})
+    tx.sign(privateKey)
+
+    const serializedTx = tx.serialize()
+    const raw = '0x' + serializedTx.toString('hex')
+
+    web3.eth.sendSignedTransaction(raw).once('transactionHash', function(hash){ console.log('txHash:', hash) })//harvest
+    .on('error', function(error){ console.log('Error:', error) })
+    .then(function(receipt){//Sell
+      console.log(receipt)
+    });
+  })
+}
+
+async function Swap(account, privateKey, contractAddress, contractABI, poolID){
+
+  const contract = new web3.eth.Contract(contractABI, contractAddress)
+  web3.eth.getTransactionCount(account, (err, txCount) => {
+
+    const txObject = {
+      nonce:    web3.utils.toHex(txCount),
+      gasLimit: web3.utils.toHex(800000), // Raise the gas limit to a much higher amount
+      gasPrice: web3.utils.toHex(web3.utils.toWei('10', 'gwei')),
+      to: contractAddress,
+      data: contract.methods.deposit(poolID, 0).encodeABI()
+    }
+
+    const tx = new Tx(txObject, {common : commonMatic})
+    tx.sign(privateKey)
+
+    const serializedTx = tx.serialize()
+    const raw = '0x' + serializedTx.toString('hex')
+
+    web3.eth.sendSignedTransaction(raw).once('transactionHash', function(hash){ console.log('txHash:', hash) })//harvest
+    .on('error', function(error){ console.log('Error:', error); return 'Failed' })
+    .then(function(receipt){//Sell
+      console.log(receipt);
+      return 'Complete'
+    });
+  })
+}
+
+//SwapFunction here
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+//Functions End
+/////////////////////////////////////////////////////////////////////////////////////////
+
+var account = '0x2c9acd7A958C129d472155ab40ae40807B2e5f9B'
+var privateKey = Buffer.from(process.env.PRIVATE_KEY, 'hex')
+
+// Read the deployed contract - PolyPUP Bone MC
+var boneMCcontractAddress = '0x9DcB2D5e7b5212fAF98e4a152827fd76bD55f68b'
+var boneMCcontractABI = [{"inputs":[{"internalType":"contract BoneToken","name":"_bone","type":"address"},{"internalType":"address","name":"_devaddr","type":"address"},{"internalType":"address","name":"_feeAddress","type":"address"},{"internalType":"uint256","name":"_BonePerBlock","type":"uint256"},{"internalType":"uint256","name":"_startBlock","type":"uint256"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":true,"internalType":"uint256","name":"pid","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"Deposit","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":true,"internalType":"uint256","name":"pid","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"EmergencyWithdraw","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":true,"internalType":"address","name":"newAddress","type":"address"}],"name":"SetDevAddress","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":true,"internalType":"address","name":"newAddress","type":"address"}],"name":"SetFeeAddress","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":false,"internalType":"uint256","name":"BonePerBlock","type":"uint256"}],"name":"UpdateEmissionRate","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":true,"internalType":"uint256","name":"pid","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"Withdraw","type":"event"},{"inputs":[],"name":"BONUS_MULTIPLIER","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"BonePerBlock","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_allocPoint","type":"uint256"},{"internalType":"contract IBEP20","name":"_lpToken","type":"address"},{"internalType":"uint16","name":"_depositFeeBP","type":"uint16"},{"internalType":"bool","name":"_withUpdate","type":"bool"}],"name":"add","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"bone","outputs":[{"internalType":"contract BoneToken","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_pid","type":"uint256"},{"internalType":"uint256","name":"_amount","type":"uint256"}],"name":"deposit","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_devaddr","type":"address"}],"name":"dev","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"devaddr","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_pid","type":"uint256"}],"name":"emergencyWithdraw","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"feeAddress","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_from","type":"uint256"},{"internalType":"uint256","name":"_to","type":"uint256"}],"name":"getMultiplier","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"pure","type":"function"},{"inputs":[],"name":"massUpdatePools","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_pid","type":"uint256"},{"internalType":"address","name":"_user","type":"address"}],"name":"pendingBone","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"contract IBEP20","name":"","type":"address"}],"name":"poolExistence","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"poolInfo","outputs":[{"internalType":"contract IBEP20","name":"lpToken","type":"address"},{"internalType":"uint256","name":"allocPoint","type":"uint256"},{"internalType":"uint256","name":"lastRewardBlock","type":"uint256"},{"internalType":"uint256","name":"accBonePerShare","type":"uint256"},{"internalType":"uint16","name":"depositFeeBP","type":"uint16"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"poolLength","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_pid","type":"uint256"},{"internalType":"uint256","name":"_allocPoint","type":"uint256"},{"internalType":"uint16","name":"_depositFeeBP","type":"uint16"},{"internalType":"bool","name":"_withUpdate","type":"bool"}],"name":"set","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_feeAddress","type":"address"}],"name":"setFeeAddress","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"startBlock","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalAllocPoint","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_BonePerBlock","type":"uint256"}],"name":"updateEmissionRate","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_pid","type":"uint256"}],"name":"updatePool","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_startBlock","type":"uint256"}],"name":"updateStartBlock","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"},{"internalType":"address","name":"","type":"address"}],"name":"userInfo","outputs":[{"internalType":"uint256","name":"amount","type":"uint256"},{"internalType":"uint256","name":"rewardDebt","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_pid","type":"uint256"},{"internalType":"uint256","name":"_amount","type":"uint256"}],"name":"withdraw","outputs":[],"stateMutability":"nonpayable","type":"function"}]
+var USDTbonePID = 14;
+
+harvestResult = await Harvest(account, privateKey, boneMCcontractAddress, boneMCcontractABI, USDTbonePID);
+
+if(harvestResult == 'Complete'){
+
+}
---
+++ Swap1InchUpdated.js
@@ -0,0 +1,128 @@
+require('dotenv').config()
+const Web3 = require('web3')
+const web3 = new Web3('https://polygon-mainnet.infura.io/v3/7a2d659fe6604d0cb3e02cd329626792')
+var Tx = require("ethereumjs-tx").Transaction
+var Common = require('ethereumjs-common').default;
+const axios = require('axios');                         //used for getting api data, install with "yarn add axios"
+
+const commonMatic = Common.forCustomChain(
+  'mainnet',
+  {
+    name: 'matic-mainnet',
+    networkId: 137,
+    chainId: 137,
+    url: 'https://rpc-mainnet.maticvigil.com/'
+  },
+  'petersburg'
+)
+
+var account = '0x2c9acd7A958C129d472155ab40ae40807B2e5f9B'
+var privateKey = Buffer.from(process.env.PRIVATE_KEY, 'hex')
+
+// Read the deployed contract - PolyPUP Bone Token
+const tokenAddress = '0x6bb45cEAC714c52342Ef73ec663479da35934bf7'
+const contractABI = [{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"delegator","type":"address"},{"indexed":true,"internalType":"address","name":"fromDelegate","type":"address"},{"indexed":true,"internalType":"address","name":"toDelegate","type":"address"}],"name":"DelegateChanged","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"delegate","type":"address"},{"indexed":false,"internalType":"uint256","name":"previousBalance","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"newBalance","type":"uint256"}],"name":"DelegateVotesChanged","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[],"name":"DELEGATION_TYPEHASH","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"DOMAIN_TYPEHASH","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"uint32","name":"","type":"uint32"}],"name":"checkpoints","outputs":[{"internalType":"uint32","name":"fromBlock","type":"uint32"},{"internalType":"uint256","name":"votes","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"subtractedValue","type":"uint256"}],"name":"decreaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"delegatee","type":"address"}],"name":"delegate","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"delegatee","type":"address"},{"internalType":"uint256","name":"nonce","type":"uint256"},{"internalType":"uint256","name":"expiry","type":"uint256"},{"internalType":"uint8","name":"v","type":"uint8"},{"internalType":"bytes32","name":"r","type":"bytes32"},{"internalType":"bytes32","name":"s","type":"bytes32"}],"name":"delegateBySig","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"delegator","type":"address"}],"name":"delegates","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"getCurrentVotes","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getOwner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"},{"internalType":"uint256","name":"blockNumber","type":"uint256"}],"name":"getPriorVotes","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"addedValue","type":"uint256"}],"name":"increaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_to","type":"address"},{"internalType":"uint256","name":"_amount","type":"uint256"}],"name":"mint","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"mint","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"nonces","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"numCheckpoints","outputs":[{"internalType":"uint32","name":"","type":"uint32"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"recipient","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"sender","type":"address"},{"internalType":"address","name":"recipient","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"}];
+
+const contract = new web3.eth.Contract(contractABI, tokenAddress)
+
+const USDTTokenAddress = '0xc2132d05d31c914a87c6611c10748aeb04b58e8f'
+
+/**
+ * The driver of the program, this will execute anything you put in it
+ */
+async function driver() {
+
+    boneBalance = await contract.methods.balanceOf(account).call()
+    let callURL = 'https://api.1inch.exchange/v3.0/137/swap?fromTokenAddress='+tokenAddress+
+    '&toTokenAddress=' + USDTTokenAddress +
+    '&amount='+boneBalance+'&fromAddress=' +
+    account +
+    '&slippage=1';
+
+    response = await axios.get('https://api.1inch.exchange/v3.0/1/tokens')
+    stdTokens = response.data
+
+    // only tokens need approval, MATIC does not
+    //begin token approval
+    if (!(tokenAddress == '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee') && tokenAddress in stdTokens) //check if this is not the native token and is std supported token
+    {
+        
+        nonce = await web3.eth.getTransactionCount(account) + 1;
+        globalData = await approveApiCaller(boneBalance, tokenAddress, nonce)
+        
+        var tx = new Tx(globalData["tx"], {common : commonMatic})
+        tx.sign(privateKey)    
+        var serializedTx = tx.serialize()
+        var raw = '0x' + serializedTx.toString('hex')
+        
+        web3.eth.sendSignedTransaction(raw).once('transactionHash', function(hash){ console.log('txHash:', hash) })
+        .on('error', function(error){ console.log('Error:', error) })
+        .then(function(receipt){
+          console.log(receipt)
+        });
+    }
+    //end token approval
+
+    //begin api call and transaction sending
+    nonce = await web3.eth.getTransactionCount(account) + 1;
+
+    globalData = await apiCaller(callURL, nonce);       //call the api to get the data, and wait until it returns
+    var tx = new Tx(globalData["tx"], {common : commonMatic})
+    tx.sign(privateKey)    
+    var serializedTx = tx.serialize()
+    var raw = '0x' + serializedTx.toString('hex')
+    
+    web3.eth.sendSignedTransaction(raw).once('transactionHash', function(hash){ console.log('txHash:', hash) })
+    .on('error', function(error){ console.log('Error:', error) })
+    .then(function(receipt){
+      console.log(receipt)
+    });
+}
+
+/**
+ * This will call the api to get an approve transaction, some tokens need to be approved to 0 before increasing again later
+ * @param {the number of tokens that are requested to be unlocked, if "null" infinite will be unlocked } value 
+ * @param {the token address of what tokens needs to be unlocked} tokenAddress
+ * @param {the nonce of the transaction} nonce
+ * @returns approve transaction data
+ */
+async function approveApiCaller(value, tokenAddress, nonce) {
+    let url = 'https://api.1inch.exchange/v3.0/1/approve/calldata' +
+        (value > -1 && value != null ? "?amount=" + value + "&" : "?") //tack on the value if it's greater than -1
+        + "tokenAddress=" + tokenAddress            //complete the called URL
+    let temp = await axios.get(url);                //get the api call
+    temp = temp.data;                               //we only want the data object from the api call
+    temp.tx.gasPrice = web3.utils.toHex(temp.tx.gasPrice);
+    temp.tx.gas = web3.utils.toHex(temp.tx.gas);
+    temp.tx["nonce"] = web3.utils.toHex(nonce); 
+    //we also need value in the form of hex
+    let val = parseInt(temp.tx["value"]);			//get the value from the transaction
+    val = '0x' + val.toString(16);				    //add a leading 0x after converting from decimal to hexadecimal
+    temp.tx["value"] = val;						    //set the value of value in the transaction object
+
+    return temp;                                    //return the data
+}
+
+/**
+ * Will call the api and return the data needed
+ * @param {the url of what api call you want} url 
+ * @param {the nonce of the transaction, the user must keep track of this} nonce
+ * @returns swap transaction
+ */
+async function apiCaller(url, nonce) {
+    let temp = await axios.get(url);                //get the api call
+    temp = temp.data;                               //we only want the data object from the api call
+    temp.tx.gasPrice = web3.utils.toHex(temp.tx.gasPrice);
+    temp.tx.gas = web3.utils.toHex(temp.tx.gas);
+
+    //we also need value in the form of hex
+    let value = parseInt(temp.tx["value"]);			//get the value from the transaction
+    value = '0x' + value.toString(16);				//add a leading 0x after converting from decimal to hexadecimal
+    temp.tx["value"] = value;						//set the value of value in the transaction object. value referrs to how many of the native token
+
+    temp.tx["nonce"] = web3.utils.toHex(nonce);                     
+    //temp.tx.chainId = 137                         //this allows the transaction to NOT be replayed on other chains, ethersjs will find it for the user
+    return temp;                                    //return the data
+}
+
+driver();
